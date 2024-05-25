af0c8b314ee200ab8ce663fd54dbb80345b6f9c7
elenaag23 <elena-alexandra.georgescu@s.unibuc.ro>
2024-05-25 12:49:25 +0300

    added login middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const verifyUser = require("../middleware/verifyUser");
+const verifyLogin = require("../middleware/verifyLogin");
 
 // Define routes for CRUD operations
 router.post("/users/register", verifyUser, usersController.register);
-router.post("/login", usersController.login);
+router.post("/login", verifyLogin, usersController.login);
 router.get("/users/get", usersController.getUsers);
 router.get("/users/getUser/:id", usersController.getUser);
 router.put("/users/edit/:id", usersController.editUser);
